eead614cb3b0dd84f4b66f9939d48ed42ffa1405
massa <zoumaniguimassa12@gmail.com>
2023-05-02 12:54:54 +0000

    Creating a new directory called components and ZoumCalculator.js and calculator.css for making design

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Affiche, Ecran } from './components/ZoumCalculator';
+import './components/Calculator.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Ecran value={0} />
+      <Affiche />
     </div>
+
   );
 }
 
