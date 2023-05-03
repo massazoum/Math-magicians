5c2801ce076608c12c1f2ed8af5269b3f0925f0f
massa <zoumaniguimassa12@gmail.com>
2023-05-03 12:30:41 +0000

    implementing fucntion handleClick

--- src/components/ZoumCalculator.js
+++ src/components/ZoumCalculator.js
@@ -1,51 +1,118 @@
 import './Calculator.css';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
-export function Affiche() {
+export default function Affiche() {
+  const [display, setDisplay] = useState('');
+
+  const handleClick = (value) => {
+    if (value === 'AC') {
+      setDisplay('');
+    } else if (value === '+/-') {
+      setDisplay((prev) => prev * -1);
+    } else if (value === '%') {
+      setDisplay((prev) => prev / 100);
+    } else if (value === '=') {
+      try {
+        const result = eval(display);
+        setDisplay(result);
+      } catch (error) {
+        setDisplay('Error');
+      }
+    } else {
+      setDisplay((prev) => prev + value);
+    }
+  };
+
   return (
     <div>
+      <div className="affichage">{display}</div>
       <div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button className="ColorBtn" type="button">/</button>
+        <button type="button" onClick={() => handleClick('AC')}>
+          AC
+        </button>
+        <button type="button" onClick={() => handleClick('+/-')}>
+          +/-
+        </button>
+        <button type="button" onClick={() => handleClick('%')}>
+          %
+        </button>
+        <button
+          className="ColorBtn"
+          type="button"
+          onClick={() => handleClick('÷')}
+        >
+          ÷
+        </button>
       </div>
       <div>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button className="ColorBtn" type="button">x</button>
+        <button type="button" onClick={() => handleClick('7')}>
+          7
+        </button>
+        <button type="button" onClick={() => handleClick('8')}>
+          8
+        </button>
+        <button type="button" onClick={() => handleClick('9')}>
+          9
+        </button>
+        <button
+          className="ColorBtn"
+          type="button"
+          onClick={() => handleClick('x')}
+        >
+          x
+        </button>
       </div>
       <div>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button className="ColorBtn" type="button">-</button>
+        <button type="button" onClick={() => handleClick('4')}>
+          4
+        </button>
+        <button type="button" onClick={() => handleClick('5')}>
+          5
+        </button>
+        <button type="button" onClick={() => handleClick('6')}>
+          6
+        </button>
+        <button
+          className="ColorBtn"
+          type="button"
+          onClick={() => handleClick('-')}
+        >
+          -
+        </button>
       </div>
       <div>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button className="ColorBtn" type="button">+</button>
+        <button type="button" onClick={() => handleClick('1')}>
+          1
+        </button>
+        <button type="button" onClick={() => handleClick('2')}>
+          2
+        </button>
+        <button type="button" onClick={() => handleClick('3')}>
+          3
+        </button>
+        <button
+          className="ColorBtn"
+          type="button"
+          onClick={() => handleClick('+')}
+        >
+          +
+        </button>
       </div>
       <div>
-        <button className="zero" type="button">0</button>
-        <button type="button">.</button>
-        <button className="ColorBtn" type="button">=</button>
+        <button className="zero" type="button" onClick={() => handleClick('0')}>
+          0
+        </button>
+        <button type="button" onClick={() => handleClick('.')}>
+          .
+        </button>
+        <button
+          className="ColorBtn"
+          type="button"
+          onClick={() => handleClick('=')}
+        >
+          =
+        </button>
       </div>
-
     </div>
   );
 }
-
-export function Ecran({ value }) {
-  return (
-    <div className="affichage">
-      {value}
-    </div>
-  );
-}
-
-Ecran.propTypes = {
-  value: PropTypes.number.isRequired,
-};
